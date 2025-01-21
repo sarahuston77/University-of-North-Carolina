76da526ae8826411cc1a7e0c0ee5756fd34303d2
shuston007 <shuston007@gmail.com>
2025-01-21 15:15:54 -0500

    Hi

---
+++ Computational Statistics/Classwork/Data Prep.R
@@ -0,0 +1,103 @@
+# Function for installing package
+install.packages("psych")
+install.packages("dplyr") # must have quotes
+
+# Function for calling it into our environment
+library(dplyr) # with or without quotes
+library(psych)
+
+# Call in data provided by a loaded package:
+data(bfi)
+
+# Check out the data:
+class(bfi)
+
+# Dimensions of data (row, col)
+dim(bfi) 
+
+# First few lines of data for the first 2 variables:
+head(bfi)[1:2]
+
+# Get the first value for variable A2
+bfi$A2[1]
+
+# Another way to get the same value:
+bfi[1,2] # [row,column]
+
+# Only get the data for those who meet a criterion
+# Here, the criterion is that a value of variable A1 equals 2
+A1is2which <- which(bfi$A1 == 2)
+A2whenA1is2 <- bfi$A2[A1is2which]
+
+# Learn about which() function: which indicies are TRUE?
+?which()
+
+# Let's find how many people had A1=2
+length(A2whenA1is2)
+
+# Another way to get the same vector
+A1is2logical <- bfi$A1 == 2
+
+# Set NA to false
+A1is2logical[which(is.na(A1is2logical))] <- FALSE
+
+# Create vector of A1 = 2
+A2whenA1is2new <- bfi$A2[A1is2logical]
+length(A2whenA1is2new)
+
+# Lets say we want to see how many people are younger than age 30 in our Bfi data.
+# using which - only has the index's this is true
+length(which(bfi$age<30))
+
+# Using logical alone: SAME length because it has T and F
+length(bfi$age<30)
+  
+# We need some clever indexing
+length(bfi$age[bfi$age<30])
+
+# Creates a vector of length 8, repeating 5
+myvector = rep(5, 8)
+myvector
+
+# Adds to each element in the vector
+myvector + 5
+
+# Vector of length 8, 1...8
+my8numbers = 1:8
+my8numbers
+myvector
+
+# Adds element wise
+myvector + my8numbers
+
+# Call in data from psych package
+data(bfi, package = "psych")
+
+# Create a new object with a new variable:
+mynewvariable = bfi$A1 + bfi$A2
+
+# Or add the variable directly to the data frame:
+bfi$sumA1A2 = bfi$A1 + bfi$A2
+
+# Check it was done right
+names(bfi)
+head(bfi)
+
+# Call in package
+library(dplyr)
+
+TSST_SR <- read.table("TSST_SelfReport.csv", header = TRUE, sep = ",") %>%
+  dplyr::select(ID, Gender, Race, Age, BMI, contains("Emo_"), contains("Sit_")) %>%
+  dplyr::mutate(Race = factor(Race, levels = c(1,2,3,4,5,6,7,8), # first variable to mutate labels = c("American Indian or Alaskan Native",
+                              "Asian American",
+                              "Native Hawaiian or other Pacific Islander",
+                              "African American",
+                              "European American",
+                              "Latin American",
+                              "More than one race",
+                              "Other"),
+# second variable to mutate:
+Gender = factor(Gender, levels = c(0,1), labels = c("Female", "Male")))
+
+?factor()
+
