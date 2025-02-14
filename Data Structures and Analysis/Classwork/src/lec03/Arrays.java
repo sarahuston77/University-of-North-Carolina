b40aaa149426348343017089f145488db16d0753
shuston007 <shuston007@gmail.com>
2025-02-14 13:41:00 -0500

    General

--- Data Structures and Analysis/Classwork/src/lec03/Arrays.java
+++ Data Structures and Analysis/Classwork/src/lec03/Arrays.java
@@ -1,8 +1,11 @@
 package lec03;
 
 public class Arrays {
+
     public static void main(String[] args){
         int[] a = {1, 2, 3, 5};
+        float[] b = new float[5];
+        System.out.println(b[4]);
         int sum = 0; // = sum of a[i]
         for (int i=0; i < a.length; i++) {
             sum += a[i];
