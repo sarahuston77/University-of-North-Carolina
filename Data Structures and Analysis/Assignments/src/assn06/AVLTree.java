d2321e8f98fc0d9375426149ffa46ae135c14af9
shuston007 <shuston007@gmail.com>
2025-04-08 12:55:36 -0400

    Started AVL tree assn

---
+++ Data Structures and Analysis/Assignments/src/assn06/AVLTree.java
@@ -0,0 +1,132 @@
+package assn06;
+
+public class AVLTree<T extends Comparable<T>> implements SelfBalancingBST<T> {
+    // Fields
+    private T _value;
+    private AVLTree<T> _left;
+    private AVLTree<T> _right;
+    private int _height;
+    private int _size;
+    
+    public AVLTree() {
+        _value = null;
+        _left = null;
+        _right = null;
+        _height = -1;
+        _size = 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    @Override
+    public int height() {
+        return _height;
+    }
+
+    @Override
+    public int size() {
+        return _size;
+    }
+
+    @Override
+    public T getValue() {
+        return _value;
+    }
+
+    @Override
+    public SelfBalancingBST<T> getLeft() {
+        if (isEmpty()) {
+            return null;
+        }
+        return _left;
+    }
+
+    @Override
+    public SelfBalancingBST<T> getRight() {
+        if (isEmpty()) {
+            return null;
+        }
+        return _right;
+    }
+
+    //==================================================================
+    // The methods below need to be completed.
+    // Refer to the SelfBalancingTree interface for the descriptions.
+    //==================================================================
+
+    /**
+     * Rotates the tree left and returns
+     * AVLTree root for rotated result.
+     */
+     private AVLTree<T> rotateLeft() {
+         // You should implement left rotation and then use this 
+         // method as needed when fixing imbalances.
+    	 // TODO
+
+         return null;
+     }
+    
+    /**
+     * Rotates the tree right and returns
+     * AVLTree root for rotated result.
+     */
+     private AVLTree<T> rotateRight() {
+         // You should implement right rotation and then use this 
+         // method as needed when fixing imbalances.
+    	 // TODO
+
+         return null;
+     }
+
+    @Override
+    public SelfBalancingBST<T> insert(T element) {
+    	// TODO
+
+        return null;
+    }
+
+    @Override
+    public SelfBalancingBST<T> remove(T element) {
+    	// TODO
+
+        return null;
+    }
+
+    @Override
+    public T findMin() {
+        if (isEmpty()) {
+            throw new RuntimeException("Illegal operation on empty tree");
+        }
+        // TODO
+
+        return null;
+    }
+
+    @Override
+    public T findMax() {
+        if (isEmpty()) {
+            throw new RuntimeException("Illegal operation on empty tree");
+        }
+        // TODO
+
+        return null;
+    }
+
+    @Override
+    public boolean contains(T element) {
+    	// TODO
+
+        return false;
+    }
+
+    @Override
+    public boolean rangeContain(T start, T end) {
+        // TODO
+
+        return false;
+    }
+
+}
